3c828e6fa791fb06f531cab8559a220d1e51a69e
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-09 20:52:25 -0500

    Agregar barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    
+    <React.Fragment>
+      <Navigation></Navigation>
+      <div className="container">
+        <h1>Hello World!</h1>
+     </div>
+    </React.Fragment>
   );
 }
 
