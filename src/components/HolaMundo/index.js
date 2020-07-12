9cfef39cfe1aee29bd7dfd4e1769c67af519c7c6
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-11 22:45:09 -0500

    Agregar componentes y react-router

--- src/components/HolaMundo/index.js
+++ src/components/HolaMundo/index.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthContext'
+import { AuthContext } from '../../contexts/AuthContext';
 
 const HolaMundo = () => {
-    const { setNombre } = useContext(AuthContext);
-    return ( 
-        <input 
-            type="text" 
-            label="Escribe tu nombre"
-            onChange={(e) => setNombre(e.target.value)}
-        ></input>
-    );
+  const { setNombre } = useContext(AuthContext);
+  return ( 
+    <input
+      onChange={(e) => setNombre(e.target.value)}
+      type="text"
+      label="Introduce tu nombre" />
+  );
 }
  
 export default HolaMundo;
---
+++ src/components/Signup/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import {
+  Button,  
+  Form,  
+  FormGroup,
+  Label,  
+  Input,
+} from 'reactstrap';
+
+const Signup = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleForm = (e) => {
+    e.preventDefault();            
+    const jsonSend = {
+      firstName,
+      lastName,
+      email,
+      password
+    };
+    console.log(jsonSend);
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className="mb-4">Signup on Eduardo's App</h1>
+      <Form onSubmit={handleForm}>
+        <FormGroup>
+          <Label>First Name</Label>
+          <Input 
+            value={firstName}
+            onChange={(e) => {setFirstName(e.target.value)}}
+            type="text"
+            id="firstName"
+            name="inputFirstName" 
+            placeholder="type your first name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Last Name</Label>
+          <Input
+            value={lastName}
+            onChange={(e) => {setLastName(e.target.value)}}
+            type="text" 
+            name="lastName" 
+            id="inputLastname" 
+            placeholder="type your last name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="type your email" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input 
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="type your password here" />
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </React.Fragment>
+  );
+}
+ 
+export default Signup;
